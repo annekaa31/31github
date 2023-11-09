24134f6919e657f86e8beba09b08604314fced6a
Anelya Kozh <anel.kozhmukhamedova@gmail.com>
2023-11-08 23:01:00 -0600

    add second solution

--- day1/simple_commad.py
+++ day1/simple_commad.py
@@ -5,6 +5,9 @@
 #The function is declared like this:
 #print('what to print')
 
-print("""Day 1 - Python Pront Function
+print("""Day 1 - Python Print Function
 The function is declared like this:
 print('what to print')""")
+
+#solution 2
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
