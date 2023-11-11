35d27db5caf8b0e7f40a5d74e8986f9c2124fd5b
Anelya Kozh <anel.kozhmukhamedova@gmail.com>
2023-11-10 22:53:23 -0600

    python primivite data type - string

---
+++ project2/data_types.py
@@ -0,0 +1,13 @@
+# data types
+#string
+# print the 1st letter in word Hello
+print("Hello"[0])
+
+# print the last letter in word Hello
+print("Hello[-1]")
+
+# another dump solution
+print("Hello"[4])
+
+# still strings
+print("123" + "988")
