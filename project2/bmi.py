65811642544646b989d81712cd6251a1827fd09a
Anelya Kozh <anel.kozhmukhamedova@gmail.com>
2023-11-12 23:57:58 -0600

    description to a program that calcumates the body mass index

---
+++ project2/bmi.py
@@ -0,0 +1,12 @@
+# 1st input: enter height in meters e.g: 1.65
+height = input()
+# 2nd input: enter weight in kilograms e.g: 72
+weight = input()
+# 🚨 Don't change the code above 👆
+
+# 1.65 72
+
+# Write your code below this line 👇
+
+weight_to_int = int(weight)
+print(type(weight_to_int))
