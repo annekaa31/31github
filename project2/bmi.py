a0611cb382e19000558d3f878b92cb17c1d53ab4
Anelya Kozh <anel.kozhmukhamedova@gmail.com>
2023-11-13 00:18:39 -0600

    solve bmi.py

--- project2/bmi.py
+++ project2/bmi.py
@@ -4,9 +4,16 @@ height = input()
 weight = input()
 # 🚨 Don't change the code above 👆
 
-# 1.65 72
+# bmi = height / weight * weight
 
 # Write your code below this line 👇
 
 weight_to_int = int(weight)
 print(type(weight_to_int))
+
+height_to_float = float(height)
+print(type(height_to_float))
+
+bmi = (weight_to_int / height_to_float ** 2)
+bmi_int = int(bmi)
+print(bmi_int)
