abbd0d8380c6953c3708d90d84e182e40cc54d13
Anelya Kozh <anel.kozhmukhamedova@gmail.com>
2023-11-28 22:34:49 -0600

    add banker roulette exercise

---
+++ project4/roulette.py
@@ -0,0 +1,10 @@
+# You are going to write a program that will select a random name from a list of# names. The person selected will have to pay for everybody's food bill.
+
+names = names_string.split(", ")
+# The code above converts the input into an array seperating
+#each name in the input by a comma and space.
+# 🚨 Don't change the code above 👆
+import random
+random_index = random.randint(1, len(names))
+random_name = names[random_index - 1]
+print(f"{random_name} is going to buy the meal today!")
