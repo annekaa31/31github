ed1fbf164bdfe156b6c933525d2055167b216b8b
Anelya Kozh <anel.kozhmukhamedova@gmail.com>
2023-12-03 20:42:45 -0600

    create jump function for reebog race

---
+++ project6/reebog.py
@@ -0,0 +1,31 @@
+def turn_around():
+    turn_left()
+    turn_left()
+    
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def draw_square():
+    move()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+for i in range(6):
+    jump()
