e4255132a2016e7c7912951bd95c083ae08bd57c
Anelya Kozh <anel.kozhmukhamedova@gmail.com>
2023-12-06 11:39:03 -0800

    combine lines for paint_area_calc

--- project7/paint_areea_calculator.py
+++ project7/paint_areea_calculator.py
@@ -1,9 +1,8 @@
 import math
 
 def paint_calc(height, width, cover):
-  num_cans = (height * width) / cover
-  round_up_cans = math.ceil(num_cans)
-  print(f"You'll need {round_up_cans} cans of paint.")
+  num_of_cans = math.ceil((height * width) / coverage )
+  print(f"You'll need {num_of_cans} cans of paint.")
 
 # Your code above this line 👆
 test_h = int(input()) # Height of wall (m)
