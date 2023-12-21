a90054373b75a0424bfe0ad5e9d43a89a40a1e47
Anelya Kozh <anel.kozhmukhamedova@gmail.com>
2023-12-20 21:07:54 -0600

    add dictionary in a dictionary

--- project9/dictionaries_task.py
+++ project9/dictionaries_task.py
@@ -18,3 +18,8 @@ travel_log = {
   "France": ["Paris", "Lille", "Dijon"],
   "Germany": ["Berlin", "Hamburg", "Stuttgart"],
 }
+
+travel_log = {
+  "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+  "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+}
