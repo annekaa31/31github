d364625175005737a47438ce5333eb1b437035c9
Anelya Kozh <anel.kozhmukhamedova@gmail.com>
2023-12-20 21:08:59 -0600

    add nesting dictionaries in a list

--- project9/dictionaries_task.py
+++ project9/dictionaries_task.py
@@ -23,3 +23,16 @@ travel_log = {
   "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
   "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
 }
+
+travel_log = [
+{
+  "country": "France", 
+  "cities_visited": ["Paris", "Lille", "Dijon"], 
+  "total_visits": 12,
+},
+{
+  "country": "Germany",
+  "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+  "total_visits": 5,
+},
+]
