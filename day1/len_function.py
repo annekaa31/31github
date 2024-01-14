fce3ec91d2c9402be4e6ac0f63f0f7c33bad7ebc
Anelya Kozh <anel.kozhmukhamedova@gmail.com>
2024-01-14 14:32:36 -0600

    remove old folder name

--- day1/len_function.py
+++
@@ -1,8 +0,0 @@
-# Provide any name in the input pane below.
-# That value can be accessed using the input() function.
-# Don't put anything inside the input() function!
-name = input()
-print(len(name))
-
-# another solution
-print(len(input()))
