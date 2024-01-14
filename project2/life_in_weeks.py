8c747cf4b1fcc4eebc7a053f77c38a917e5f6924
Anelya Kozh <anel.kozhmukhamedova@gmail.com>
2024-01-14 14:35:14 -0600

    remove old folders

--- project2/life_in_weeks.py
+++
@@ -1,7 +0,0 @@
-age = input()
-# 🚨 Don't change the code above 👆
-# Write your code below this line 👇
-total_weeks = 90 * 52
-spent_weeks = int(age) * 52
-left_weeks = (total_weeks - spent_weeks)
-print(f"You have {left_weeks} weeks left.")
