470b44f8b08bfad69d04bf94da11f8a44630f0c0
Anelya Kozh <anel.kozhmukhamedova@gmail.com>
2024-01-14 14:20:37 -0600

    add input and variables exercise

---
+++ input_variables/simple_commad.py
@@ -0,0 +1,17 @@
+#Write a program that prints the following notes on Python programming 
+#into the Output console. Your code should print all three lines.
+
+#Day 1 - Python Print Function
+#The function is declared like this:
+#print('what to print')
+
+print("""Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')""")
+
+#solution 2
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
+
+# print Hello, your_name
+#use string concatenation
+print("Hello, " + "Anelya")
