be161118c1256d2009c92e68817a9312c967108a
Anelya Kozh <anel.kozhmukhamedova@gmail.com>
2024-01-23 22:42:53 -0600

    add operator variable

--- assignments/programming_tasks/basic_calculator.py
+++ assignments/programming_tasks/basic_calculator.py
@@ -1,8 +1,18 @@
 print("Welcome to basic Calculator program that performs basic arithmetic operations between two numbers.")
 num1 = int(input("Please write a first number: "))
 num2 = int(input("Please write a second number: "))
+operator = input("Please specify the math operation here (+, -, *, /, %) : ")
 addition = num1 + num2
 subtraction = num1 - num2
 multiplication = num1 * num2
 division = round(num1 / num2, 2)
-print(f"The addition is: {addition}, the subtraction is: {subtraction}, the multiplication is: {multiplication}, the division is: {division}")
+if operator == "+":
+    print(f"{num1} + {num2} = {addition}")
+elif operator == "-":
+    print(f"{num1} - {num2} = {subtraction}")
+elif operator == "*":
+    print(f"{num1} * {num2} = {multiplication}")
+elif operator == "/":
+    print(f"{num1} / {num2} = {division}")
+else:
+    print("Sorry, I can't understand your operation")
