557c7fe1a0f08bc833359b6510c046167a8c71cc
Anelya Kozh <anel.kozhmukhamedova@gmail.com>
2024-01-27 13:43:29 -0600

    clean up

--- assignments/matrix_w_random_numbers.py
+++
@@ -1,7 +0,0 @@
-import numpy
-import numpy as np
-
-r = int(input("Please type number if rows in your matrix: "))
-c = int(input("Please type number if colomn in your matrix: "))
-m = np.random.randint(100, size = (r,c))
-print(m)
