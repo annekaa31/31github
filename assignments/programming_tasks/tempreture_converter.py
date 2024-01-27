59407e425aa70423fffc2dfde5e17228de8d8c81
Anelya Kozh <anel.kozhmukhamedova@gmail.com>
2024-01-26 23:42:01 -0600

    draft version Temperature Converte

--- assignments/programming_tasks/tempreture_converter.py
+++ assignments/programming_tasks/tempreture_converter.py
@@ -1,6 +1,12 @@
-fahrenheit = int(input("Write temperature in Fahrenheit: "))
-celsius = int(input("Write temperature in Celsius: "))
-fahrenheit_to_celsius = round(5/9 * (fahrenheit - 32))
-celsius_to_fahrenheit = round(5/9 * (fahrenheit - 32))
-print(celsius_to_fahrenheit)
-print(fahrenheit_to_celsius)
+temperature = input("Write temperature that you want to convert: ")
+# a = temperature[len(temperature)-1]
+# print(a)
+if temperature[len(temperature)-1] == "f":
+    temperature = int(temperature.rstrip(temperature[-1]))
+    result = round(5/9 * (temperature - 32))
+    print(f"{result} celsius")
+elif temperature[len(temperature)-1] == "c":
+    temperature = int(temperature.rstrip(temperature[-1]))
+    # print(temperature)
+    result = round(temperature * (9/5) + 32)
+    print(f"{result} fahrenheit")
