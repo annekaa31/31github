7684924f52c87b53ee42f1897db3202c48840f80
Anelya Kozh <anel.kozhmukhamedova@gmail.com>
2024-01-28 23:47:14 -0600

    add count vowels in a string draft version

---
+++ assignments/programming_tasks/count_vowels.py
@@ -0,0 +1,7 @@
+def countvowels(string):
+    num_vowels = 0
+    for char in string:
+        if char in "aeiouAEIOU":
+           num_vowels = num_vowels + 1
+    return num_vowels
+print(countvowels("kitty"))
