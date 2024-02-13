5d67a30a9327d1f029141ed6a11cbcd02b9d1c6d
Anelya Kozh <anel.kozhmukhamedova@gmail.com>
2024-02-13 14:49:46 -0600

    add another solution for pizza order program

--- if_else_logical_operators/pizza_order.py
+++ if_else_logical_operators/pizza_order.py
@@ -39,3 +39,25 @@ if size == "L":
   else:
     pizza_price
   print(f"Your final bill is: ${pizza_price}.")
+
+
+# Another yet solution
+
+# bill = 0
+# if size == "S":
+#   bill += 15
+# elif size == "M":
+#   bill += 20
+# elif size == "L":
+#   bill += 25
+
+# if add_pepperoni == "Y":
+#   if size == "S":
+#     bill += 2
+#   else:
+#     bill += 3
+
+# if extra_cheese == "Y":
+#   bill +=1
+
+# print(f"Your final bill is: ${bill}.")
