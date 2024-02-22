9c39744e61e5cfe7797a0d2402e48f891afc3e51
Anelya Kozh <anel.kozhmukhamedova@gmail.com>
2024-02-22 16:41:57 -0600

    add clean solution for average_height.py

--- python_loops/average_height.py
+++ python_loops/average_height.py
@@ -5,18 +5,16 @@ for n in range(0, len(student_heights)):
 # 🚨 Don't change the code above 👆
   
 # Write your code below this row 👇
+
 total_height = 0
-num_of_students = 0
-for i in student_heights:
-  total_height += i
-  num_of_students += 1
+for height in student_heights:
+  total_height += height
+  
+total_students = 0
+for number in student_heights:
+  total_students += 1
+  
+average_height = round(total_height / total_students)
 print(f"total height = {total_height}")
-print(f"number of students = {num_of_students}")
-
-# num_of_students = 0
-# for j in student_heights:
-#   num_of_students += 1
-# print(f"number of students = {num_of_students}")
-
-average_height = round(total_height / num_of_students)
+print(f"number of students = {total_students}")
 print(f"average height = {average_height}")
