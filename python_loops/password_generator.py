52dd4539fec0aa8f37d873eb1cba41358f29e9ac
Anelya Kozh <anel.kozhmukhamedova@gmail.com>
2024-02-24 21:00:36 -0600

    add another solution with a list for password generator

--- python_loops/password_generator.py
+++ python_loops/password_generator.py
@@ -27,3 +27,26 @@ for num in range(1, nr_numbers + 1):
 password_shuffled = "".join(random.sample(password, len(password)))
 
 print(f"Here is your password: {password_shuffled}")
+
+
+#anotheer solution w/ list
+# password_list = []
+# 
+# for char in range(1, nr_letters + 1):
+#   password_list.append(random.choice(letters))
+# 
+# for char in range(1, nr_symbols + 1):
+#   password_list += random.choice(symbols)
+# 
+# for char in range(1, nr_numbers + 1):
+#   password_list += random.choice(numbers)
+# 
+# print(password_list)
+# random.shuffle(password_list)
+# print(password_list)
+# 
+# password = ""
+# for char in password_list:
+#   password += char
+# 
+# print(f"Your password is: {password}")
