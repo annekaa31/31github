e1347842a88f9bbb6b9b698c32ba29175fc5db1c
Anelya Kozh <anel.kozhmukhamedova@gmail.com>
2024-02-28 23:48:06 -0600

    add lower higher start code

---
+++ assignments/lower_higher.py
@@ -0,0 +1,13 @@
+import random
+
+def get_random_account():
+  """Get data from random account"""
+  return random.choice(data)
+
+def format_data(account):
+  """Format account into printable format: name, description and country"""
+  name = account["name"]
+  description = account["description"]
+  country = account["country"]
+  # print(f'{name}: {account["follower_count"]}')
+  return f"{name}, a {description}, from {country}"
